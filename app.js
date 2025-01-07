ea2609aafa8c75efc382d89a878c5a26fe6799b1
runaround <runaroundontheground@gmail.com>
2025-01-07 10:15:16 -0700

    made things happen, and wrote down some notes

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 import { fileURLToPath } from "url";
 import path from "path";
 import express from "express";
-import { json } from "stream/consumers";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.port || 3000;
+const users = [];
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -20,8 +20,59 @@ app.get("/", (req, res) => {
 });
 app.get("/getCurrentDate", (req, res) => {
   const date = new Date();
-  res.json({ date: date });
+  const text = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+  res.json({ date: text });
 });
+app.post("/addPerson", (req, res) => {
+  console.log("test");
+  const user = req.body;
+  if (user.firstName && user.lastName) {
+    users.push(user);
+  }
+  res.json({ user: user });
+});
+app.get("/getPeople", (req, res) => {
+  res.json({ users: users });
+});
+app.post("/getPerson", (req, res) => {
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].firstName === req.body) {
+      res.redirect("/");
+      break;
+    }
+  }
+  res.redirect("/");
+});
+
+// see documentation on expressjs for more detail
+
+// something also useful to know
+// fetch can have different methods, like post as well
+
+// routing
+// app.method(path, handler)
+// route paths
+// app.get('/ab?cd') match a, optional character, then cd, ex acd, abcd aicd
+// app.get('/ab*cd') match ab, anything can be between, then ends in cd
+// app.get(/.*fly$/) anything that ends with fly, $ means nothing after, .* means any amount of any character before following rules
+/* 
+  "/users/:parameter/stuff/:anotherParameter"
+  allows things like req.params.parameter and req.params.anotherParameter
+  multiple parameters in the same location
+  "/users/:parameter-:anotherParameter"
+
+  multiple methods on the same route path
+  app.route("/url")
+    .get((req, res) => {
+      stuff
+    })
+    .post((req, res) => {
+      stuff
+    })
+    .put((req, res) => {
+      stuff
+    })
+*/
 
 app.listen(port, () => {
   console.log(`running server from port ${port}`);
--- public/index.js
+++ public/index.js
@@ -1,7 +1,27 @@
 const dateContainer = document.getElementById("dateContainer");
 const peopleContainer = document.getElementById("peopleContainer");
+const addPersonButton = document.getElementById("addPersonButton");
+const searchOutput = document.getElementById("searchOutput");
+const addPersonForm = document.getElementById("addPersonForm");
 
-const date = new Date();
-dateContainer.innerText = `${
-  date.getMonth() + 1
-}/${date.getDate()}/${date.getFullYear()}`;
+addPersonForm.addEventListener("submit", (ev) => {
+  ev.preventDefault();
+});
+
+async function getCurrentDate() {
+  await fetch("/getCurrentDate")
+    .then((response) => response.json())
+    .then((data) => (dateContainer.innerText = data.date));
+}
+async function getPeople() {
+  await fetch("/getPeople")
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        peopleContainer.innerText = JSON.stringify(data.users);
+      }
+    });
+}
+
+getCurrentDate();
+getPeople();
